2d1405ebfa21f80eca582db13394515df221e4fd
Zaki Arrozi Arsyad <zakiarsyad@gmail.com>
2021-09-20 21:08:56 +0700

    remove React classes

--- index.js
+++ index.js
@@ -14,13 +14,5 @@ const response = [
     {
         'nama-kelas': 'Javascript Intermediate',
         'harga': 100000
-    },
-    {
-        'nama-kelas': 'React Dasar',
-        'harga': 200000
-    },
-    {
-        'nama-kelas': 'React Intermediate',
-        'harga': 200000
     }
 ]
