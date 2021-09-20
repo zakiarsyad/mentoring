81cd253d6863839419d35e54401b5acafd10b4f5
Zaki Arrozi Arsyad <zakiarsyad@gmail.com>
2021-09-20 21:08:07 +0700

    add a new file

---
+++ index.js
@@ -0,0 +1,26 @@
+const response = [
+    {
+        'nama-kelas': 'HTML Dasar',
+        'harga': 100000
+    },
+    {
+        'nama-kelas': 'CSS Dasar',
+        'harga': 100000
+    },
+    {
+        'nama-kelas': 'Javascript Dasar',
+        'harga': 100000
+    },
+    {
+        'nama-kelas': 'Javascript Intermediate',
+        'harga': 100000
+    },
+    {
+        'nama-kelas': 'React Dasar',
+        'harga': 200000
+    },
+    {
+        'nama-kelas': 'React Intermediate',
+        'harga': 200000
+    }
+]
