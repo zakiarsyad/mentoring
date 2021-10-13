bb784543ecddc48fb7ebfa6406caea66bc4a7277
zakiarsyad <zakiarsyad@gmail.com>
2021-10-14 06:51:42 +0700

    add tailwind example

---
+++ tailwind/custom-css/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Source Code Pro"', "monospace"],
+        heading: ['"Source Code Pro"', "monospace"],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
